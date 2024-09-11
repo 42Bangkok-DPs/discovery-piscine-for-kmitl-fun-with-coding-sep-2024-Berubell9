e62fe4f070075a9b1b80e387fcc7d78cfb610e32
Klinglunlun <79560837+Berubell9@users.noreply.github.com>
2024-09-11 21:50:33 +0700

    Add files via upload

---
+++ cell03/ex04/ex03bis/todo.js
@@ -0,0 +1,39 @@
+let TodoList = [];
+
+function createTodo(text) {
+    return $('<div class="todo"></div>').html('<p>' + text + '</p>' + '<button onclick="remove(\'' + text + '\')">x</button>');
+}
+
+function render() {
+    $('#ft_list').empty();
+    $.each(TodoList, function(index, element) {
+        $('#ft_list').append(createTodo(element));
+    });
+    document.cookie = JSON.stringify(TodoList);
+}
+
+function newTodo() {
+    let name = prompt("Name the todo.");
+    if (name.length > 0) {
+        TodoList.unshift(name);
+        render();
+    }
+}
+
+function remove(text) {
+    let yes = confirm('Are you sure to remove.');
+    if (yes) {
+        TodoList = $.grep(TodoList, function(a) {
+            return a != text;
+        });
+        render();
+    }
+}
+
+$(document).ready(function() {
+    let save = document.cookie;
+    if (save.length > 0) {
+        TodoList = JSON.parse(save);
+        render();
+    }
+});
